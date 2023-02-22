5d6f4f4d283c5985c7c4d0a0d4fbd85bb486f027
Panagiotis <panagiotispapadeas1@gmail.com>
2023-02-22 16:44:13 +0200

    multiple source

--- flink/flinkconsumer.py
+++ flink/flinkconsumer.py
@@ -7,7 +7,7 @@ from pyflink.common.watermark_strategy import TimestampAssigner
 from pyflink.common import WatermarkStrategy, Encoder, Types, SimpleStringSchema, Time, Duration
 from pyflink.datastream import StreamExecutionEnvironment, RuntimeExecutionMode
 from pyflink.datastream.connectors.file_system import FileSource, StreamFormat, FileSink, OutputFileConfig, RollingPolicy
-from pyflink.datastream.connectors.kafka import KafkaSource, KafkaOffsetsInitializer
+from pyflink.datastream.connectors.kafka import KafkaSource, KafkaOffsetsInitializer, KafkaTopicPartition
 from pyflink.datastream.window import TumblingEventTimeWindows, TumblingProcessingTimeWindows
 
 class KafkaRowTimestampAssigner(TimestampAssigner):
@@ -21,7 +21,15 @@ class KafkaRowTimestampAssigner(TimestampAssigner):
         #print(final)
         return final
 
-def word_count(input_path, output_path):
+def my_map_func(line):
+    x = []
+    for i in line.split():      
+        x.append(i)
+    temp = x[1] + " " + x[2]    
+    y = (x[0], temp, float(x[3]))  
+    return y  
+     
+def live_streaming_layer(input_path, output_path):
     env = StreamExecutionEnvironment.get_execution_environment()
     env.set_runtime_mode(RuntimeExecutionMode.STREAMING)
     # write all the data to one file
@@ -36,36 +44,94 @@ def word_count(input_path, output_path):
         #     watermark_strategy=WatermarkStrategy.for_monotonous_timestamps(),
         #     source_name="file_source"
         # )
+    partition_set = {
+        KafkaTopicPartition("dailyAggr", 0),
+    }
+    
+    partition_set1 = {
+        KafkaTopicPartition("dailyAggr", 1),
+    }
+
+    partition_set2 = {
+        KafkaTopicPartition("dailyAggr", 2),
+    }
+
+    partition_set3 = {
+        KafkaTopicPartition("dailyAggr", 3),
+    }
+
     env.add_jars("file:///home/panoplos/apache/flink-sql-connector-kafka-1.16.0.jar")
-    source = KafkaSource.builder().set_bootstrap_servers('localhost:9092').set_topics("TutorialTopic").set_group_id("my-group").set_starting_offsets(KafkaOffsetsInitializer.earliest()).set_value_only_deserializer(SimpleStringSchema()).build()
+    source = KafkaSource.builder() \
+        .set_bootstrap_servers('localhost:9092') \
+        .set_partitions(partition_set) \
+        .set_group_id("my-group") \
+        .set_starting_offsets(KafkaOffsetsInitializer.earliest()) \
+        .set_value_only_deserializer(SimpleStringSchema()) \
+        .build()
+
+    source1 = KafkaSource.builder() \
+        .set_bootstrap_servers('localhost:9092') \
+        .set_partitions(partition_set1) \
+        .set_group_id("my-group") \
+        .set_starting_offsets(KafkaOffsetsInitializer.earliest()) \
+        .set_value_only_deserializer(SimpleStringSchema()) \
+        .build()    
+
+    source2 = KafkaSource.builder() \
+        .set_bootstrap_servers('localhost:9092') \
+        .set_partitions(partition_set2) \
+        .set_group_id("my-group") \
+        .set_starting_offsets(KafkaOffsetsInitializer.earliest()) \
+        .set_value_only_deserializer(SimpleStringSchema()) \
+        .build()
+
+    source3 = KafkaSource.builder() \
+        .set_bootstrap_servers('localhost:9092') \
+        .set_partitions(partition_set3) \
+        .set_group_id("my-group") \
+        .set_starting_offsets(KafkaOffsetsInitializer.earliest()) \
+        .set_value_only_deserializer(SimpleStringSchema()) \
+        .build()   
+         
     ds = env.from_source(source, WatermarkStrategy.no_watermarks(), "Kafka Source")
-
-    # def split(line):
-    #     yield from line.split()
-
-    def my_map_func(line):
-        x = []
-        for i in line.split():      
-            x.append(i)
-        temp = x[1] + " " + x[2]    
-        y = (x[0], temp, float(x[3]))  
-        return y  
-     
-
-    # compute word count
-    #ds = ds.flat_map(split)
     ds = ds.map(my_map_func, output_type=Types.TUPLE([Types.STRING(), Types.STRING(), Types.FLOAT()]))
+
+    ds1 = env.from_source(source1, WatermarkStrategy.no_watermarks(), "Kafka Source1")
+    ds1 = ds1.map(my_map_func, output_type=Types.TUPLE([Types.STRING(), Types.STRING(), Types.FLOAT()]))
+
+    ds2 = env.from_source(source2, WatermarkStrategy.no_watermarks(), "Kafka Source2")
+    ds2 = ds2.map(my_map_func, output_type=Types.TUPLE([Types.STRING(), Types.STRING(), Types.FLOAT()]))
+
+    ds3 = env.from_source(source3, WatermarkStrategy.no_watermarks(), "Kafka Source3")
+    ds3 = ds3.map(my_map_func, output_type=Types.TUPLE([Types.STRING(), Types.STRING(), Types.FLOAT()]))
 
     with_timestamp_and_watermarks = ds.filter(lambda e: e).assign_timestamps_and_watermarks(WatermarkStrategy.for_bounded_out_of_orderness(Duration.of_seconds(1)).with_timestamp_assigner(KafkaRowTimestampAssigner()))
 
-
+    with_timestamp_and_watermarks1 = ds1.filter(lambda e: e).assign_timestamps_and_watermarks(WatermarkStrategy.for_bounded_out_of_orderness(Duration.of_seconds(1)).with_timestamp_assigner(KafkaRowTimestampAssigner()))
+    
+    with_timestamp_and_watermarks2 = ds2.filter(lambda e: e).assign_timestamps_and_watermarks(WatermarkStrategy.for_bounded_out_of_orderness(Duration.of_seconds(1)).with_timestamp_assigner(KafkaRowTimestampAssigner()))
+    
+    with_timestamp_and_watermarks3 = ds3.filter(lambda e: e).assign_timestamps_and_watermarks(WatermarkStrategy.for_bounded_out_of_orderness(Duration.of_seconds(1)).with_timestamp_assigner(KafkaRowTimestampAssigner()))
+    
     result = with_timestamp_and_watermarks.key_by(lambda i: i[0]) \
                .window(TumblingEventTimeWindows.of(Time.seconds(3600))) \
-               .reduce(lambda v1, v2: (v1[0], v1[1], v1[2] + v2[2]), output_type=Types.TUPLE([Types.STRING(), Types.STRING(), Types.FLOAT()])) \
+               .reduce(lambda v1, v2: (v1[0], v1[1], v1[2] + v2[2]), output_type=Types.TUPLE([Types.STRING(), Types.STRING(), Types.FLOAT()]))
 
-        # .map(lambda i: (i, 1), output_type=Types.TUPLE([Types.STRING(), Types.INT()]))
-        # .key_by(lambda i: i[0]) \
-        # .reduce(lambda i, j: (i[0], i[1] + j[1]))
+    result1 = with_timestamp_and_watermarks1.key_by(lambda i: i[0]) \
+               .window(TumblingEventTimeWindows.of(Time.seconds(3600))) \
+               .reduce(lambda v1, v2: (v1[0], v1[1], v1[2] + v2[2]), output_type=Types.TUPLE([Types.STRING(), Types.STRING(), Types.FLOAT()]))
+
+    result2 = with_timestamp_and_watermarks2.key_by(lambda i: i[0]) \
+               .window(TumblingEventTimeWindows.of(Time.seconds(3600))) \
+               .reduce(lambda v1, v2: (v1[0], v1[1], v1[2] + v2[2]), output_type=Types.TUPLE([Types.STRING(), Types.STRING(), Types.FLOAT()]))
+
+    result3 = with_timestamp_and_watermarks3.key_by(lambda i: i[0]) \
+               .window(TumblingEventTimeWindows.of(Time.seconds(3600))) \
+               .reduce(lambda v1, v2: (v1[0], v1[1], v1[2] + v2[2]), output_type=Types.TUPLE([Types.STRING(), Types.STRING(), Types.FLOAT()]))                      
+        
+    # .map(lambda i: (i, 1), output_type=Types.TUPLE([Types.STRING(), Types.INT()]))
+    # .key_by(lambda i: i[0]) \
+    # .reduce(lambda i, j: (i[0], i[1] + j[1]))
 
     # define the sink
     # if output_path is not None:
@@ -85,6 +151,9 @@ def word_count(input_path, output_path):
     print("Printing result to stdout. Use --output to specify output path.")
     #ds.print()
     result.print()
+    result1.print()
+    result2.print()
+    result3.print()
 
     # submit for execution
     env.execute()
@@ -108,4 +177,4 @@ if __name__ == '__main__':
     argv = sys.argv[1:]
     known_args, _ = parser.parse_known_args(argv)
 
-    word_count(known_args.input, known_args.output)
+    live_streaming_layer(known_args.input, known_args.output)
--- kafka_scripts/start.sh
+++ kafka_scripts/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-/usr/local/kafka/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic TutorialTopic
+/usr/local/kafka/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 10 --topic dailyAggr
 
-/usr/local/kafka/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic asynchronous
+/usr/local/kafka/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic lateREj
 
 ./../../apache/flink/flink-1.16.1/bin/start-cluster.sh
